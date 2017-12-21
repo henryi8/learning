bc664d5d4dde92b4f6c5991ab0ad8c176fedee0e
zhaoxny <347910189@qq.com>
2017-12-21 10:47:25 +0800

    ..

--- src/com/single/ESingle.java
+++ src/com/single/ESingle.java
@@ -5,6 +5,7 @@ package com.single;
  * @date 2017/12/20
  * 单例模式-->饿汉模式
  *  一加载这个类，就把这个实例初始化完成，能保证geteSingle的时候，这个单例在之前就已经存在。
+ *  http://www.wisdomdd.cn/Wisdom/resource/articleDetail.htm?resourceId=181
  */
 public class ESingle {
 
