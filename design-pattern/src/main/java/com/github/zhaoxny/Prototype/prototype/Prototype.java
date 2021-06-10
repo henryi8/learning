ccd3932675c4ca8b1f92ae5c299c048a794647f5
henry <347910189@qq.com>
2021-06-10 13:12:42 +0800

    2021年5月19日 16:49:26 add：原型模式

---
+++ design-pattern/src/main/java/com/github/zhaoxny/Prototype/prototype/Prototype.java
@@ -0,0 +1,9 @@
+package com.github.zhaoxny.Prototype.prototype;
+
+/**
+ * 原型模式
+ * @author henry
+ * @date 2021/6/5 18:37
+ */
+public class Prototype {
+}
--- design-pattern/src/main/java/com/github/zhaoxny/SingletonPattern/registerSingleton/Conainer.java
+++ design-pattern/src/main/java/com/github/zhaoxny/SingletonPattern/registerSingleton/Conainer.java
@@ -1,7 +1,7 @@
 package com.github.zhaoxny.SingletonPattern.registerSingleton;
 
 /**
- * spring
+ * spring容器单例模式
  * @author henry
  * @date 2021/5/30 11:46
  */
